825323cd7e96820a1fab393c3929ba99cbe4cc79
Bogdan Dobre <bogdandobre@Bogdans-MacBook-Pro.local>
2023-05-05 16:23:10 +0300

    added rswag for my comment controller

---
+++ spec/requests/comments_spec.rb
@@ -0,0 +1,28 @@
+require 'swagger_helper'
+
+describe 'Comments API' do
+  path '/boards/1/stories/1/comments' do
+    post 'Creates a comment' do
+      tags 'Comments'
+      security [bearerAuth: []]
+      consumes 'application/json'
+      parameter name: :comment, in: :body, schema: {
+        type: :object,
+        properties: {
+          body: { type: :string }
+        },
+        required: [ 'body' ]
+      }
+
+      response '201', 'comment created' do
+        let(:comment) { { body: 'Test comment' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:comment) { { body: nil } }
+        run_test!
+      end
+    end
+  end
+end
